f3da55b47d7eeaff633e9b230c5e68d3e1a1383e
Ismail Atthar <aniaman2@gmail.com>
2022-12-10 07:23:07 +0530

    Chebyshev distance

--- utils.mjs
+++ utils.mjs
@@ -1,5 +1,8 @@
 export const sum = array => array.reduce((a, b) => a + b, 0);
 
-export const manhattanDistance = (point1, point2) => Math.max(
+export const chebyshevDistance = (point1, point2) => Math.max(
     Math.abs(point1.x - point2.x), Math.abs(point1.y - point2.y)
 );
+
+export const manhattanDistance = (point1, point2) =>
+    Math.abs(point1.x - point2.x) + Math.abs(point1.y - point2.y);
